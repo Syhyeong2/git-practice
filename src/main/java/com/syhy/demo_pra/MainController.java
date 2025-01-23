320d5d9c3534dc7d60ba2e7fc91ae51ec3987893
sihyeong <syhy7051@gmail.com>
2025-01-23 17:34:59 +0900

    second commit

---
+++ src/main/java/com/syhy/demo_pra/MainController.java
@@ -0,0 +1,14 @@
+package com.syhy.demo_pra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
+
