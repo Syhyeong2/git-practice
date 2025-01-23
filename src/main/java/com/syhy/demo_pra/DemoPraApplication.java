21775dab1f5aa8b3b94dd263e124afc970c8a5cf
sihyeong <syhy7051@gmail.com>
2025-01-23 17:22:14 +0900

    first commit

---
+++ src/main/java/com/syhy/demo_pra/DemoPraApplication.java
@@ -0,0 +1,13 @@
+package com.syhy.demo_pra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syhy/demo_pra/DemoPraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syhy.demo_pra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
